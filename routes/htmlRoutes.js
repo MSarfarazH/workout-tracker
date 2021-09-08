129123b52d18a55f20872a39ef64c93a489396d5
Sarfaraz <m.s.hussain@outlook.com>
2021-09-08 12:38:19 -0500

    changes to html routes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,15 +1,15 @@
 const router = require("express").Router()
 const path = require("path");
 
-  router.get("/exercise",function (req,res){   
+  router.get("/exercise",function (req, res){   
       res.sendFile(path.join(__dirname,"../public/exercise.html"));
   });
 
-  router.get("/",function(req,res){    
+  router.get("/",function(req, res){    
       res.sendFile(path.join(__dirname,"../public/index.html"));
   });
 
-  router.get("/stats",function(req,res){   
+  router.get("/stats",function(req, res){   
       res.sendFile(path.join(__dirname,"../public/stats.html"));
   });
 
